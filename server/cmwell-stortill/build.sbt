5203dda195c54bed23b9f5ae250473e65d20b687
Yaakov Breuer <Yaakov.Breuer@thomsonreuters.com>
2018-12-02 10:42:16 +0200

    Adding HTTPS Script for CAS change

--- server/cmwell-stortill/build.sbt
+++ server/cmwell-stortill/build.sbt
@@ -4,8 +4,14 @@ libraryDependencies ++= {
   val dm = dependenciesManager.value
   Seq(
     dm("com.typesafe.akka", "akka-stream"),
-    dm("com.lightbend.akka", "akka-stream-alpakka-cassandra")
+    dm("com.lightbend.akka", "akka-stream-alpakka-cassandra"),
+    dm("org.rogach", "scallop")
   )
 }
+
+packMain := Map(
+  "addProtocol"    -> "cmwell.operations.modifier.AddProtocolField",
+  "verifyProtocol" -> "cmwell.operations.modifier.VerifyProtocolField",
+)
 
 fullTest := (test in Test).value
---
+++ server/cmwell-stortill/src/main/scala/cmwell/operations/modifier/ModifierOperation.scala
@@ -0,0 +1,72 @@
+/**
+  * Copyright 2015 Thomson Reuters
+  *
+  * Licensed under the Apache License, Version 2.0 (the “License”); you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  *   http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+  * an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  *
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  */
+package cmwell.operations.modifier
+
+import cmwell.driver.{Dao, DaoExecution}
+import cmwell.util.concurrent.SimpleScheduler
+import com.datastax.driver.core.{ConsistencyLevel, PreparedStatement, ResultSet}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.DurationInt
+import org.rogach.scallop.ScallopConf
+
+//scalastyle:off
+trait ModifierOperation {
+  type UUID = String
+  class Conf(arguments: Seq[String]) extends ScallopConf(arguments) { val host = opt[String]("host", required = true); verify() }
+
+
+
+  val statement: String
+  val task: (ResultSet, UUID) => Unit = (_,_) => ()
+
+
+
+  def main(args: Array[String]): Unit = {
+    val conf = new Conf(args)
+
+    val dao = Dao(clusterName = "", "data2", conf.host())
+    val pStmt = dao.getSession.prepare(statement)
+    val executor = new Executor(pStmt)(dao)
+
+    var c = 0
+    val timer = SimpleScheduler.scheduleAtFixedRate(0.second, 500.millis) { println(s" >>> $c items processed") }
+    println("\n\n >>> Executing...")
+    scala.io.Source.stdin.getLines().foreach { uuid =>
+      executor.exec(uuid).map(task(_,uuid))
+      c += 1
+    }
+
+    timer.cancel()
+    println(" >>> Done, shutting down connection...\n")
+    dao.shutdown()
+  }
+}
+
+object AddProtocolField extends ModifierOperation {
+  override val statement = "INSERT INTO data2.infoton (uuid, quad, field, value) VALUES (?, 'cmwell://meta/sys', 'protocol', 'https');"
+}
+
+object VerifyProtocolField extends ModifierOperation {
+  override val statement = "SELECT value FROM data2.infoton WHERE uuid=? AND quad='cmwell://meta/sys' AND field='protocol';"
+  override val task: (ResultSet, String) => Unit = (rs, uuid) =>
+    if(rs.isExhausted || rs.one().getString("value")!="https") println(s"[OOPS] $uuid was not verified!")
+}
+
+class Executor(pStatement: PreparedStatement)(implicit dao: Dao) extends DaoExecution {
+  def exec(uuid: String) =
+    executeAsyncInternal(pStatement.bind(uuid).setConsistencyLevel(ConsistencyLevel.QUORUM))
+}
+//scalastyle:on
